24c1ac7c19f5c1c4cc72a73e75a99768e6441dd1
nikita <hekutok@gmail.com>
2024-05-23 00:37:23 +0300

    strange analog input behaviour

--- airplane/airplane.ino
+++ airplane/airplane.ino
@@ -7,49 +7,59 @@ int y;
 Servo motor;
 RF24 radio(9,10);
 byte address[][6] = {"1Node", "2Node", "3Node", "4Node", "5Node", "6Node"}; //возможные номера труб
-
-void setup() {
-   //INIT MOTOR
-  motor.attach(A2);
-  motor.writeMicroseconds(2300);
-  delay(2000);
-  myservo.writeMicroseconds(2000);
-  delay(6000);
-
-  // INIT RADIO
-  radio.begin();
-  delay(3000);
-  radio.setAutoAck(1);        // режим подтверждения приёма, 1 вкл 0 выкл
-  radio.setRetries(0, 15);    // (время между попыткой достучаться, число попыток)
-  radio.enableAckPayload();   // разрешить отсылку данных в ответ на входящий сигнал
-  radio.setPayloadSize(32);
-  radio.setPALevel(RF24_PA_HIGH);
-  radio.setDataRate (RF24_250KBPS); // скорость обмена. На выбор RF24_2MBPS, RF24_1MBPS, RF24_250KBPS
-  radio.setChannel(0x60);             // выбираем канал (в котором нет шумов!)
-
-  radio.startListening();
-
- 
-
-  //INIT SERVO
-}
-
 struct Plane{
   uint16_t throttle;    // Газ
   uint16_t roll;        // Крен
   uint16_t pitch;       // Тангаж
   uint16_t yaw;         // Рысканье
-}
+};
 Plane plane;
+
+
+void setup() {
+  Serial.begin(9600);
+   //INIT MOTOR
+  motor.attach(A2);
+  motor.writeMicroseconds(2300);
+  delay(2000);
+  motor.writeMicroseconds(800);
+  delay(6000);
+  motor.writeMicroseconds(800);
+
+  // INIT RADIO
+  radio.begin();
+  delay(3000);
+  radio.setAutoAck(1);        // режим подтверждения приёма, 1 вкл 0 выкл
+  radio.enableAckPayload();   // разрешить отсылку данных в ответ на входящий сигнал
+  radio.setRetries(10, 30);    // (время между попыткой достучаться, число попыток)
+  radio.setPayloadSize(sizeof(Plane));
+  radio.setPALevel(RF24_PA_MIN);
+  radio.setDataRate (RF24_250KBPS); // скорость обмена. На выбор RF24_2MBPS, RF24_1MBPS, RF24_250KBPS
+  radio.openReadingPipe(1, address[0]);   // хотим слушать трубу 0
+  radio.setChannel(0x60);             // выбираем канал (в котором нет шумов!)
+
+  radio.startListening();
+  //INIT SERVO
+}
+
+
 
 
 void loop() {
   while(radio.available()){
     radio.read(&plane, sizeof(plane));
+    Serial.print("throttle:");
+    Serial.println(plane.throttle);
+    // Serial.print("roll:");
+    // Serial.println(plane.roll);
+    // Serial.print("pitch:");
+    // Serial.println(plane.pitch);
+    // Serial.print("yaw:");
+    // Serial.println(plane.yaw);
   }
-   motor.write(plane.throttle);
-  
-
+  if (plane.throttle <= 2250 || plane.throttle >= 800)
+    motor.write(plane.throttle);
+  delay(100);
 }
 
 
--- controller/controller.ino
+++ controller/controller.ino
@@ -7,43 +7,53 @@ int y;
 RF24 radio(9,10);
 byte address[][6] = {"1Node", "2Node", "3Node", "4Node", "5Node", "6Node"}; //возможные номера труб
 
+struct Plane{
+  uint16_t throttle;    // Газ
+  uint16_t roll;        // Крен
+  uint16_t pitch;       // Тангаж
+  uint16_t yaw;         // Рысканье
+};
+
+Plane plane;
+
+
 void setup() {
   Serial.begin(9600);
   // INIT RADIO
   radio.begin();
   delay(3000);
   radio.setAutoAck(1);        // режим подтверждения приёма, 1 вкл 0 выкл
-  radio.setRetries(0, 15);    // (время между попыткой достучаться, число попыток)
+  radio.setRetries(10, 30);    // (время между попыткой достучаться, число попыток)
   radio.enableAckPayload();   // разрешить отсылку данных в ответ на входящий сигнал
-  radio.setPALevel(RF24_PA_MAX);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.setDataRate (RF24_250KBPS); // скорость обмена. На выбор RF24_2MBPS, RF24_1MBPS, RF24_250KBPS
+
   radio.stopListening();
   radio.openWritingPipe(address[0]);
   radio.setChannel(0x60);             // выбираем канал (в котором нет шумов!)
 
-  if (radio.write("123",3)){
-    Serial.write("Successfully connected");
+  if (radio.write(&plane,sizeof(Plane))){
+    Serial.println("Successfully connected");
+    delay(3000);
   }else{
-    Serial.write("Not connected !");
+    Serial.println("Not connected !");
+    delay(5000);
   }
     
   radio.setPayloadSize(sizeof(Plane));
-  radio.openWritingPipe();
+  radio.openWritingPipe(address[0]);
 
 }
-struct Plane{
-  uint16_t throttle;    // Газ
-  uint16_t roll;        // Крен
-  uint16_t pitch;       // Тангаж
-  uint16_t yaw;         // Рысканье
-}
-Plane plane;
 
 void loop() {
-    plane.throttle = constrain(map(analogRead(A1),500,1023,800,2200),800,2250);
+    // plane.throttle = constrain(map(analogRead(A1),525,1023,800,2250),800,2250);
+    plane.throttle = analogRead(A0);
     plane.roll = 0;
     plane.pitch = 0;
     plane.yaw = 0;
-    radio.write(plane, sizeof(Plane));
+    // radio.write(&plane, sizeof(Plane));
     Serial.print("throttle:");
     Serial.println(plane.throttle);
+    delay(100);
+    
 }
